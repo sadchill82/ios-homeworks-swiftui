fbb2ddd8ab03978b6b2cc7cbde1e3f6127e165e4
“Islam <nurmuhametoffislam@yandex.ru>
2024-08-14 00:21:30 +0300

    Initial Commit

---
+++ Navigation_SwiftUI/Navigation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_SwiftUIApp.swift
+//  Navigation_SwiftUI
+//
+//  Created by Ислам on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
